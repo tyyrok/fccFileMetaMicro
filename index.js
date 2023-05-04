6bc810409ad3b4f3ba8c7ea96e9feda75cc9be38
Pavel Zubko <tyyrok@gmail.com>
2023-05-04 07:19:34 +0000

    feat: add multer and configured json output

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 
 var app = express();
 
+const multer = require('multer');
+const upload = multer({ dest: "./public/" });
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +14,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res, next){
+  console.log(req.file, req.body);
+  res.json({ name: req.file.originalname, 
+             type: req.file.mimetype,
+             size: req.file.size, })
+  next();
+} );
 
 
 const port = process.env.PORT || 3000;
